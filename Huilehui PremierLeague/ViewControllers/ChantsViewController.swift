ef7aa7ed92e988623fc71f0a5697473062a87442
Sankrub <navathon.l@ku.th>
2024-03-17 02:15:42 +0700

    adding chant audio after pressed play button

--- Huilehui PremierLeague/ViewControllers/ChantsViewController.swift
+++ Huilehui PremierLeague/ViewControllers/ChantsViewController.swift
@@ -24,6 +24,7 @@ class ChantsViewController: UIViewController {
     }()
     
     private lazy var teamsViewModel = TeamsViewModel()
+    private lazy var audioManagerViewModel = AudioManagerViewModel()
     
     // MARK: Lifecycle
     
@@ -78,7 +79,7 @@ extension ChantsViewController: UITableViewDataSource {
 
 extension ChantsViewController: TeamTableViewCellDelegate{
     func didTapPlayback(for team: Team) {
-        
+        audioManagerViewModel.playback(team)
         teamsViewModel.togglePlayback(for: team)
         tableView.reloadData()
     }
---
+++ Huilehui PremierLeague/ViewModel/AudioManagerViewModel.swift
@@ -0,0 +1,29 @@
+//
+//  AudioManagerViewModel.swift
+//  Huilehui PremierLeague
+//
+//  Created by Navathon Limamapar on 17/3/2567 BE.
+//
+
+import Foundation
+import AVKit
+
+class AudioManagerViewModel {
+    private var chantAudioPlayer: AVAudioPlayer?
+    
+    func playback(_ team: Team){
+        if team.isPlaying { // if play the stop
+            chantAudioPlayer?.stop()
+        } else { // if not play then play
+            guard let path = Bundle.main.path(forResource: "\(team.id.chantFile).mp3", ofType: nil)  else { return } // use guard to be when there is no file will not crash
+            let url = URL(fileURLWithPath: path)
+            do {
+                chantAudioPlayer = try AVAudioPlayer(contentsOf: url)
+                chantAudioPlayer?.numberOfLoops = -1 // -1 loop forever
+                chantAudioPlayer?.play()
+            } catch {
+                print(error)
+            }
+        }
+    }
+}
